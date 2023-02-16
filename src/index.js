791f707a7e94414c9378b022e9a1c98f0ab91b6a
victor <82973437+chipingo42@users.noreply.github.com>
2023-02-17 00:46:55 +0100

    updated to main pet api

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "Detailspage/:productId",
+    path: "Detailspage/:petId",
     element: <Detailspage />,
   },
 ]);
