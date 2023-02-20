46fff379ff8b0fb92912980f6d5b6b0f725267a2
victor <82973437+chipingo42@users.noreply.github.com>
2023-02-20 23:45:52 +0100

    complete adoptme page

--- src/Page/AdoptLuna.jsx
+++ src/Page/AdoptLuna.jsx
@@ -9,14 +9,14 @@ const AdoptLuna = ({ visible, onClose }) => {
 
   return (
     <div id='luna' className='bg-[#FFFFFFDE] min-h-screen w-screen fixed top-0 z-40 flex justify-center items-start' onClick={editwindow}>
-      <div className='absolute  top-[30%] w-[451px] h-[300px] bg-[#FFFFFF] rounded-md shadow-xl flex flex-col justify-center items-center'>
-        <h1 className='font-Inter font-[500] text-[24px] leading-[25px] text-[#344062] '>Are you sure you want to</h1>
-        <h1 className='font-Inter font-[500] text-[24px] leading-[25px] text-[#344062]'>adopt <span className='text-[#F15827]'>Luna</span>?</h1>
-        <div className='flex justify-center space-x-3 mt-[30px] '>
-          <button className='w-[154px] h-[46px] bg-[#0D75FF] flex justify-center items-center rounded-sm'>
+      <div className='absolute  top-[30%] w-[451px] h-[300px] bg-[#FFFFFF] rounded-md shadow-xl flex flex-col justify-center items-center sm:w-[330px]'>
+        <h1 className='font-Inter font-[500] text-[24px] leading-[25px] text-[#344062] sm:text-[20px] '>Are you sure you want to</h1>
+        <h1 className='font-Inter font-[500] text-[24px] leading-[25px] text-[#344062] sm:text-[20px]'>adopt <span className='text-[#F15827]'>Luna</span>?</h1>
+        <div className='flex justify-center space-x-3 mt-[30px] sm:space-x-5 '>
+          <button className='w-[154px] h-[46px] bg-[#0D75FF] flex justify-center items-center rounded-sm sm:w-[120px]'>
             <span className='font-Inter font-[500] text-[14px] text-[#FFFFFF] '>Adopt Luna</span>
           </button>
-          <button className='w-[154px] h-[46px] bg-[#F3F5FB] flex justify-center items-center rounded-sm'>
+          <button className='w-[154px] h-[46px] bg-[#F3F5FB] flex justify-center items-center rounded-sm sm:w-[120px]'>
             <span className='font-Inter font-[500] text-[14px] text-[#58667E] ' onClick={onClose}>Cancel</span>
           </button>
         </div>
